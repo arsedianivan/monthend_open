2e031f8d83491800ffd1e0df790be87ba835c223
Arsedian Ivanurrahman <arsedianivanurrahman@192-168-1-101.tpgi.com.au>
2024-06-24 13:57:20 +1000

    Add environment variables

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
+
+DATABASE_URL = os.getenv("postgresql://username:password@hostname:port/database")
+SECRET_KEY = os.getenv("321caa270a527f3795258c7d6484cad2")
 
 @app.get("/")
 def read_root():
