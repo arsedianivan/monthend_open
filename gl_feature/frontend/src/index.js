f802f8774b384ef100d128228a730ef7edb684f4
arsedianivan <arsedian.ivan@gmail.com>
2024-07-03 19:50:26 +1000

    Implement Client-Side Logging

--- gl_feature/frontend/src/index.js
+++ gl_feature/frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import log from 'loglevel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,14 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if (process.env.NODE_ENV === 'development') {
+  log.setLevel('debug');
+} else {
+  log.setLevel('warn');
+}
+
+log.info('Application started');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
